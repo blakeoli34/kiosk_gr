52e7978c7511f575d3220ce93f7f9b7e28c28334
Kaylee Maddox <Kaylee@Kaylees-MacBook-Pro.local>
2016-05-02 17:52:37 -0400

    secondary template started.

---
+++ calendar_month.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Kiosk GR</title>
+	<link rel="stylesheet" href="css/global.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="css/glyphs.css" type="text/css" media="screen">
+	<!--[if IE]><script type="text/javascript" src="js/ie.js"></script><![endif]-->
+</head>
+<body>
+  <div class="page2">
+    <?php include("inc/header.php"); ?> 
+	<div class="focus">
+    	<img alt="" src="" />
+	</div>
+	<div class="buttonbar">
+	</div>
+	<div class="row">
+		<h1 class="center">Sport Events</h1>
+	</div>
+	<div class="row">
+    	<div class="col-margin fill"></div>
+		<div class="col-5 fill">
+        	<div class="calsquare">03</div>
+        	<h5>April</h5>
+            <h4>Sunday</h4>
+		</div>
+		<div class="col-3 fill gold">
+		</div>
+        <div class="col-margin fill"></div>	
+	</div>
+	<div class="row">
+		<div class="col-9 fill red">
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-4 fill gold">
+		</div>
+		<div class="col-5 fill gray">
+		</div>
+	</div>	
+  </div>
+  <?php include("inc/btn.php"); ?>
+  <?php include("inc/overlay.php"); ?>
+  <?php include("js/scripts.php"); ?> 
+</body>
+</html>
