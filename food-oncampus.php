da82c1055115ec8d27c4dd19cf09e7a348daf3f4
Hannah Winkler <winkleh2@ferris.edu>
2016-05-03 20:54:14 -0400

    added food pages

---
+++ food-oncampus.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Kiosk GR</title>
+	<link rel="stylesheet" href="css/global.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="css/glyphs.css" type="text/css" media="screen">
+	<!--[if IE]><script type="text/javascript" src="js/ie.js"></script><![endif]-->
+</head>
+<body>
+  <div class="page2">
+    <?php include("inc/header.php"); ?> 
+	<div class="focus wheretoeat">
+	</div>
+	<div class="buttonbar">
+	</div>
+	<div class="breadcrumbs">
+		<a href="index.php">HOME</a> > <a href="whereto_eat.php">FOOD</a> > <a class="active" href="food-oncampus.php">ON CAMPUS</a>
+	</div>	
+	<div class="row">
+		<h1 class="center">On Campus</h1>
+	</div>
+    <div class="row">
+		<div id="thequad" class="col-9 filltall gold">
+			<h3>THE QUAD CAFE</h3>
+		</div>
+	</div>
+     <div class="row">
+		<div id="oncampus" class="col-9 filltall red">
+			<h3>THE ROCK</h3>
+		</div>
+	</div>
+	<div class="row">
+		<div id="starbucks" class="col-half filltall gold">
+			<h3>STARBUCKS</h3>
+		</div>
+		<div id="themarket" class="col-half filltall darkgrey">
+			<h3>THE MARKET</h3>
+		</div>
+	</div>
+  </div>
+  <?php include("inc/btn.php"); ?>
+  <?php include("inc/overlay.php"); ?>
+  <?php include("js/scripts.php"); ?> 
+</body>
+</html>
---
+++ starbucks.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Kiosk GR</title>
+	<link rel="stylesheet" href="css/global.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="css/glyphs.css" type="text/css" media="screen">
+	<!--[if IE]><script type="text/javascript" src="js/ie.js"></script><![endif]-->
+</head>
+<body>
+  <div class="page2">
+    <?php include("inc/header.php"); ?> 
+	<div class="focus wheretoeat">
+	</div>
+	<div class="buttonbar">
+	</div>
+	<div class="breadcrumbs">
+		<a href="index.php">HOME</a> > <a href="events.php">ON CAMPUS</a> > <a class="active" href="starbucks.php">STARBUCKS</a>
+	</div>	
+	<div class="row">
+		<h1 class="center">Starbucks</h1>
+	</div>
+	
+	<div class="row">
+    	<div class="col-margin fill"></div>
+		<div class="col-5">
+     		<div class="col-9">   
+                <div class="col-margin fill"></div>
+                <div class="col-65 horizontal_line">
+                        <p class="event_headline">The University Ceneter</p>
+                        <p class="event_location">Hours: 7:00AM-9:00PM<br>Monday - Friday</p>
+                        <p class="event_location">10:00AM until 9:00PM<br>Saturday & Sunday</p>
+                        <p class="event_details">map ></p>
+                </div>
+            </div>    
+           
+            <div class="col-margin fill"></div>
+			<div class="col-65 horizontal_line">
+                        <p class="event_headline">Business/IRC</p>
+                        <p class="event_location">Hours: 7:30AM-7:00PM<br>Monday - Thursday</p>
+                        <p class="event_location">7:30AM until 2:30PM<br>Friday</p>
+                        <p class="event_location">Closed Saturday and Sunday</p>
+                        <p class="event_details">map ></p>
+                </div>
+           
+			</div>
+            <div class="col-1 fill"></div>
+            <div class="col-3">
+            	<img src="images/starbucks_logo.png">
+            </div>
+		</div>	
+	  </div>		
+  </div>
+  <?php include("inc/btn.php"); ?>
+  <?php include("inc/overlay.php"); ?>
+  <?php include("js/scripts.php"); ?> 
+</body>
+</html>
